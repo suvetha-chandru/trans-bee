ea3e6ff2c2a00e2f557431f2f9636b7b3d4c72c6
baranidharang <77617148+baranidharang@users.noreply.github.com>
2025-08-18 21:29:18 +0530

    edited commit

--- lib/screens/home_content.dart
+++ lib/screens/home_content.dart
@@ -9,20 +9,21 @@ class HomeContent extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          // Background gradient in blue shades
+          // Background gradient
           Container(
             decoration: const BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
                 colors: [
-                  Color(0xFFB3E5FC), // Light blue
-                  Color(0xFF81D4FA), // Sky blue
-                  Color(0xFF4FC3F7), // Medium blue
+                  Color(0xFFB3E5FC),
+                  Color(0xFF81D4FA),
+                  Color(0xFF4FC3F7),
                 ],
               ),
             ),
           ),
+
           // Floating decorative circles
           const Positioned(
             top: 50,
@@ -40,35 +41,58 @@ class HomeContent extends StatelessWidget {
             child: AnimatedCircle(radius: 30, color: Colors.blueAccent),
           ),
 
-          // Animated images on top-left and top-right corners
-          const Positioned(
+          // Top-left and top-right images (minimal/no animation)
+          Positioned(
             top: 0,
             left: 0,
-            child: AnimatedImageBox(
+            child: SizedBox(
               width: 100,
               height: 100,
-              image: 'assets/tl.png',
+              child: Image.asset('assets/tl.png'), // static image
             ),
           ),
-          const Positioned(
+          Positioned(
             top: 0,
             right: 0,
-            child: AnimatedImageBox(
+            child: SizedBox(
               width: 100,
               height: 100,
-              image: 'assets/tr.png',
+              child: Image.asset('assets/tr.png'), // static image
             ),
           ),
 
+          // Main content
           Center(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const SizedBox(height: 80), // spacing from top images
+                  const SizedBox(height: 80),
 
-                  // Welcome Text with dark blue shade
+                  // Top animated image before Welcome text
+                  TweenAnimationBuilder(
+                    tween: Tween<double>(begin: 0, end: 1),
+                    duration: const Duration(seconds: 2),
+                    curve: Curves.easeOut,
+                    builder: (context, double value, child) {
+                      return Opacity(
+                        opacity: value,
+                        child: Transform.translate(
+                          offset: Offset(0, 50 * (1 - value)),
+                          child: SizedBox(
+                            width: 150,
+                            height: 150,
+                            child: Image.asset('assets/top_animated.jpeg'),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+
+                  const SizedBox(height: 30),
+
+                  // Welcome Text
                   TweenAnimationBuilder(
                     tween: Tween<double>(begin: 0, end: 1),
                     duration: const Duration(seconds: 1),
@@ -82,7 +106,7 @@ class HomeContent extends StatelessWidget {
                             style: TextStyle(
                               fontSize: 34,
                               fontWeight: FontWeight.bold,
-                              color: Colors.blue.shade900, // dark blue
+                              color: Colors.blue.shade900,
                               shadows: [
                                 Shadow(
                                   blurRadius: 15,
@@ -124,7 +148,7 @@ class HomeContent extends StatelessWidget {
                   ),
                   const SizedBox(height: 40),
 
-                  // Animated "Book Now" button
+                  // Book Now button
                   TweenAnimationBuilder(
                     tween: Tween<double>(begin: 0, end: 1),
                     duration: const Duration(milliseconds: 1500),
@@ -171,7 +195,7 @@ class HomeContent extends StatelessWidget {
   }
 }
 
-// Animated floating circle widget
+// Floating animated circles remain the same
 class AnimatedCircle extends StatefulWidget {
   final double radius;
   final Color color;
@@ -209,68 +233,6 @@ class _AnimatedCircleState extends State<AnimatedCircle>
               decoration: BoxDecoration(
                 color: widget.color.withOpacity(0.3),
                 shape: BoxShape.circle,
-              ),
-            ),
-          );
-        });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-}
-
-// Animated Image Box Widget
-class AnimatedImageBox extends StatefulWidget {
-  final double width;
-  final double height;
-  final String image;
-  const AnimatedImageBox(
-      {super.key, required this.width, required this.height, required this.image});
-
-  @override
-  State<AnimatedImageBox> createState() => _AnimatedImageBoxState();
-}
-
-class _AnimatedImageBoxState extends State<AnimatedImageBox>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller =
-        AnimationController(vsync: this, duration: const Duration(seconds: 3))
-          ..repeat(reverse: true);
-    _animation = Tween<double>(begin: 0, end: 20).animate(
-        CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-        animation: _animation,
-        builder: (context, child) {
-          return Transform.translate(
-            offset: Offset(0, _animation.value),
-            child: Container(
-              width: widget.width,
-              height: widget.height,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                image: DecorationImage(
-                  image: NetworkImage(widget.image),
-                  fit: BoxFit.cover,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.blue.withOpacity(0.3),
-                      blurRadius: 8,
-                      offset: const Offset(2, 2)),
-                ],
               ),
             ),
           );
